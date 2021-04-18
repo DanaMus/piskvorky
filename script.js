e28c1e72f3988ea4ed6eed7ccd0d4f2302514836
Dana Musilova <dana.musilova@email.cz>
2021-04-18 17:52:26 +0200

    Nastaveno střídání hráčů

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 let activePlayer = 'circle';
 const buttonElm = document.querySelectorAll('.square');
 const playElm = document.querySelector('.gamein');
+const playerElm = document.querySelector('.player');
+
+const Player = () => {
+  return `
+    <p class="player-description">Hraje: <img src="${activePlayer}.svg" alt="${activePlayer}" class="${activePlayer} white" /></p>
+  `;
+};
 
 for (let i = 1; i < buttonElm.length; i++) {
   buttonElm[i].addEventListener('click', () => {
@@ -15,10 +22,13 @@ for (let i = 1; i < buttonElm.length; i++) {
       if (activePlayer === 'circle') {
         buttonElm[i].classList.add('square-circle');
         buttonElm[i].setAttribute('disabled', true);
+        activePlayer = 'cross';
       } else {
         buttonElm[i].classList.add('square-cross');
         buttonElm[i].setAttribute('disabled', true);
+        activePlayer = 'circle';
       }
+      playerElm.innerHTML = Player();
     }
   });
 }
